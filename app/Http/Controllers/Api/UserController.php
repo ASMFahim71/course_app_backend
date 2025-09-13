91f3a8d4b5eaa68c0188dd35e7a578a9ad4962a5
ASMFahim71 <fahimuddin270@gmail.com>
2025-09-13 20:47:40 +0600

    Initialize Laravel project structure with essential files and configurations

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
+
+class UserController extends Controller
+{
+    /**
+     * Create User\
+     * @param Request $request
+     * @return User 
+     */
+    public function login(Request $request)
+    {
+        
+        
+        
+        try {
+            //Validated
+            $validateUser = Validator::make(
+                $request->all(),
+                [
+                    'avatar' => 'required',
+                    'type' => 'required',
+                    'open_id' => 'required',
+                    'name' => 'required',
+                    'email' => 'required',
+                  //  'password' => 'required'
+                ]
+            );
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            $validated = $validateUser->validated();
+            $map = [];
+            $map['type'] = $validated['type'];
+            $map['open_id'] = $validated['open_id'];
+
+            $user = User::where($map)->first();
+           
+           
+            if (empty($user->id)) {
+                $validated["token"] = md5(uniqid() . rand(1000, 9999));
+                $validated['created_at'] = Carbon::now();
+              //  $validated['password'] = Hash::make($validated['password']);
+                $userID = User::insertGetId($validated);
+                $userInfo = User::where('id', '=', $userID)->first();
+                $accesstoken = $userInfo->createToken(uniqid())->plainTextToken;
+                $userInfo->access_token = $accesstoken;
+                User::where('id', '=', $userID)->update(['access_token' => $accesstoken]);
+
+                return response()->json([
+                    'code' => 200,
+                    'msg' => 'User Created Successfully',
+                    'data' => $userInfo
+                ], 200);
+            }
+
+            $accesstoken = $user->createToken(uniqid())->plainTextToken;
+            $user->access_token = $accesstoken;
+            User::where('id', '=', $user->id)->update(['access_token' => $accesstoken]);
+
+            return response()->json([
+                'code' => 200,
+                'msg' => 'Userlogged in Successfully',
+                'data' => $user
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => 500,
+                'msg' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+}
